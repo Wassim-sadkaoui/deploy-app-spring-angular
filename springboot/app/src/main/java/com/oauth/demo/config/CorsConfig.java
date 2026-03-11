942292b9b47699498085c2ee3dbafb26347c8631
Wassim Sadkaoui <sadkaouiwassim@gmail.com>
2026-03-11 22:57:10 +0100

    fix backend

--- springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
+++ springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.187.9:4200") // Add your allowed origins here
+                .allowedOrigins("http://localhost:4200",
+                "http://127.0.0.1:4200") // Add your allowed origins here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
